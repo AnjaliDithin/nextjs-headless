088b751af8d2ba1b061d34445243359a1bc4f22d
Anjali K P <anjali.kp@acodez.co.in>
2025-11-12 19:20:34 +0530

    second commit

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,12 +1,128 @@
 
-import React from 'react';
-import { client } from '@/lib/graphql-client';
-import { gql } from 'graphql-request';
-import Link from 'next/link';
-import '../../styles/_hero-banner.scss';
-import '../../styles/_col-3-blog.scss';
-import '../../styles/_highlight-blog.scss';
-import Headingblock from '../components/Headingblock';
+// import React from 'react';
+// import { client } from '@/lib/graphql-client';
+// import { gql } from 'graphql-request';
+// import Link from 'next/link';
+// import '../../styles/_hero-banner.scss';
+// import '../../styles/_col-3-blog.scss';
+// import '../../styles/_highlight-blog.scss';
+// import Headingblock from '../components/Headingblock';
+
+// export default async function Blog() {
+//   const query = gql`
+//     {
+//       posts(first: 12) {
+//         nodes {
+//           id
+//           title
+//           date
+//           slug
+//           excerpt
+//           featuredImage {
+//             node {
+//               sourceUrl
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `;
+
+//   const data = await client.request(query);
+//   const posts = data.posts.nodes;
+
+//   return (
+//     <main>
+//       {/* Hero Banner Section */}
+//       <section className="hero-banner-sec sub-banner-sec">
+//         <div className="fluidcontainer">
+//           <div className="hero-banner-area slideshow">
+//             <div className="hero-banner-wrapper">
+//               <div className="slide">
+//                 <div className="slide-image">
+//                   <img
+//                     className="sub-bnr-img"
+//                     src="/assets/blogs/banner.jpg"
+//                     width="1300"
+//                     height="520"
+//                     alt="Gallery"
+//                     title="Gallery"
+//                   />
+//                 </div>
+//                 <h1 className="slide-subtitle">Blogs</h1>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+
+//       {/* Blog Grid Section */}
+//       <section className="col3-blog-sec scroll-up-gridwrap">
+//         <div className="heading-mainblock without-content">
+//           <div className="container">
+//               <Headingblock
+//                   items={[
+//                       {
+//                       heading: "Blogs"
+//                       },
+//                   ]}
+//                   align="center"
+//                   whiteText={false}
+//               />
+//           </div>
+//         </div>
+
+//         <div className="grid-blog-area">
+//           <div className="container">
+//             <div className="col3-gridarea">
+//               {posts.map((post) => (
+//                 <div key={post.id} className="blog-item scroll-griditem">
+//                   <Link href={`/blog/${post.slug}`}>
+//                     <div className="top-bar">
+//                       {post.featuredImage?.node?.sourceUrl && (
+//                         <img
+//                           src={post.featuredImage.node.sourceUrl}
+//                           width="431"
+//                           height="200"
+//                           alt={post.title}
+//                         />
+//                       )}
+//                     </div>
+//                     <div className="btm-bar">
+//                       <div className="title-tag" dangerouslySetInnerHTML={{ __html: post.title }} />
+//                       <div className="date">
+//                         {new Date(post.date).toLocaleDateString()}
+//                       </div>
+//                       <div
+//                         className="content"
+//                         dangerouslySetInnerHTML={{ __html: post.excerpt }}
+//                       />
+//                       <div className="btn-wrap">
+//                         <button className="common-black-btn">
+//                           <span>Read More</span>
+//                         </button>
+//                       </div>
+//                     </div>
+//                   </Link>
+//                 </div>
+//               ))}
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </main>
+//   );
+// }
+import { gql } from "graphql-request";
+import { client } from "@/lib/graphql-client";
+import Link from "next/link";
+import Headingblock from "../components/Headingblock";
+
+import "../../styles/_hero-banner.scss";
+import "../../styles/_col-3-blog.scss";
+import "../../styles/_highlight-blog.scss";
+
+export const revalidate = 60; // ISR: re-fetch every 60s
 
 export default async function Blog() {
   const query = gql`
@@ -28,8 +144,14 @@ export default async function Blog() {
     }
   `;
 
-  const data = await client.request(query);
-  const posts = data.posts.nodes;
+  let posts = [];
+
+  try {
+    const data = await client.request(query);
+    posts = data?.posts?.nodes || [];
+  } catch (error) {
+    console.error("❌ GraphQL Fetch Error:", error);
+  }
 
   return (
     <main>
@@ -60,52 +182,57 @@ export default async function Blog() {
       <section className="col3-blog-sec scroll-up-gridwrap">
         <div className="heading-mainblock without-content">
           <div className="container">
-              <Headingblock
-                  items={[
-                      {
-                      heading: "Blogs"
-                      },
-                  ]}
-                  align="center"
-                  whiteText={false}
-              />
+            <Headingblock
+              items={[{ heading: "Blogs" }]}
+              align="center"
+              whiteText={false}
+            />
           </div>
         </div>
 
         <div className="grid-blog-area">
           <div className="container">
             <div className="col3-gridarea">
-              {posts.map((post) => (
-                <div key={post.id} className="blog-item scroll-griditem">
-                  <Link href={`/blog/${post.slug}`}>
-                    <div className="top-bar">
-                      {post.featuredImage?.node?.sourceUrl && (
-                        <img
-                          src={post.featuredImage.node.sourceUrl}
-                          width="431"
-                          height="200"
-                          alt={post.title}
+              {posts.length === 0 ? (
+                <p style={{ textAlign: "center", padding: "50px" }}>
+                  No posts found. Check your WordPress GraphQL endpoint.
+                </p>
+              ) : (
+                posts.map((post) => (
+                  <div key={post.id} className="blog-item scroll-griditem">
+                    <Link href={`/blog/${post.slug}`}>
+                      <div className="top-bar">
+                        {post.featuredImage?.node?.sourceUrl && (
+                          <img
+                            src={post.featuredImage.node.sourceUrl}
+                            width="431"
+                            height="200"
+                            alt={post.title}
+                          />
+                        )}
+                      </div>
+                      <div className="btm-bar">
+                        <div
+                          className="title-tag"
+                          dangerouslySetInnerHTML={{ __html: post.title }}
                         />
-                      )}
-                    </div>
-                    <div className="btm-bar">
-                      <div className="title-tag" dangerouslySetInnerHTML={{ __html: post.title }} />
-                      <div className="date">
-                        {new Date(post.date).toLocaleDateString()}
+                        <div className="date">
+                          {new Date(post.date).toLocaleDateString()}
+                        </div>
+                        <div
+                          className="content"
+                          dangerouslySetInnerHTML={{ __html: post.excerpt }}
+                        />
+                        <div className="btn-wrap">
+                          <button className="common-black-btn">
+                            <span>Read More</span>
+                          </button>
+                        </div>
                       </div>
-                      <div
-                        className="content"
-                        dangerouslySetInnerHTML={{ __html: post.excerpt }}
-                      />
-                      <div className="btn-wrap">
-                        <button className="common-black-btn">
-                          <span>Read More</span>
-                        </button>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              ))}
+                    </Link>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
@@ -113,3 +240,5 @@ export default async function Blog() {
     </main>
   );
 }
+
+
--- src/lib/graphql-client.js
+++ src/lib/graphql-client.js
@@ -1,14 +1,10 @@
-// import { GraphQLClient } from 'graphql-request';
 
-// const endpoint = 'http://nextjs-headless.local/graphql'; 
-
-// export const client = new GraphQLClient(endpoint);
 import { GraphQLClient } from 'graphql-request';
 
 // Use environment variable first, fallback to local for dev
 const endpoint =
   process.env.NEXT_PUBLIC_WP_GRAPHQL_URL ||
-  'http://nextjs-headless.local/graphql';
+  'https://headless-nextui.wuaze.com/graphql';
 
 export const client = new GraphQLClient(endpoint);
 
